b927ccfe2cf44bfb1476e6f62363f9a957ef945a
ganeshamushak <ganeshamushak@gmail.com>
2025-03-14 09:14:56 +0530

    updated common CTA and SEO details

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Enterprise Migration Expert | Cloud & Legacy Modernization – Jitan Gupta';
-export const SITE_DESCRIPTION = 'Struggling with outdated systems? I help businesses migrate to scalable, cloud-native architectures. Reduce costs & boost efficiency. Book a free strategy call!';
+export const SITE_DESCRIPTION = 'Outdated systems slowing you down? I help enterprises migrate to scalable, cloud-native architectures. Unlock efficiency & cut costs—Book a free strategy call!';
 export const SITE_URL = "https://jitangupta.com/";
