21aa27c65e88d62ecedf437846eb7d1440988645
Jitan Gupta <gtangupta@gmail.com>
2025-03-14 08:58:47 -0700

    Migration expert (#5)
    * changed hero section
    
    * added how can I help
    
    * code cleanup
    
    * updated home page
    
    * updated common CTA and SEO details
    
    * worked on "enterprise-migration-consultation" page

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jitan Gupta';
-export const SITE_DESCRIPTION = 'Jitan Gupta – Full-Stack Engineer, Mentor, and Content Creator. Learn full-stack development, software architecture, debugging, and cloud solutions. Grow your tech career with expert insights, tutorials, and hands-on guidance.';
+export const SITE_TITLE = 'Enterprise Migration Expert | Cloud & Legacy Modernization – Jitan Gupta';
+export const SITE_DESCRIPTION = 'Outdated systems slowing you down? I help enterprises migrate to scalable, cloud-native architectures. Unlock efficiency & cut costs—Book a free strategy call!';
 export const SITE_URL = "https://jitangupta.com/";
