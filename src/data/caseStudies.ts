7b77029a9de71c1329552d96fa7bbd01689107ef
Jitan Gupta <gtangupta@gmail.com>
2025-03-15 11:01:36 -0700

    Migration expert (#6)
    * changed hero section
    
    * added how can I help
    
    * code cleanup
    
    * updated home page
    
    * updated common CTA and SEO details
    
    * worked on "enterprise-migration-consultation" page
    
    * code cleanup
    
    ---------
    
    Co-authored-by: ganeshamushak <ganeshamushak@gmail.com>

---
+++ src/data/caseStudies.ts
@@ -0,0 +1,8 @@
+export const CaseStudies = [
+    {
+      image: "/case-study/case-study-placeholder.png", // Replace with actual image URL
+      title: "Migrating Microsoft Membership to Centralized Identity",
+      achievement: "Coming Soon",
+      link: "#"
+    }
+];
